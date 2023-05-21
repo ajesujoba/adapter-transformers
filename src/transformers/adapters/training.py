32c47d7ff906bed75f8bdc3bf34d4167ee76182e
Jesujoba Alabi <jalabi@ws710lx.lsv.uni-saarland.de>
2023-05-21 21:53:29 +0200

    enforce removal of adapters at layers

--- src/transformers/adapters/training.py
+++ src/transformers/adapters/training.py
@@ -37,7 +37,6 @@ def setup_adapter_training(
     model,
     adapter_args: AdapterArguments,
     adapter_name: str,
-    overwrite4adp: Optional[bool] = False,
     adapter_config_kwargs: Optional[dict] = None,
     adapter_load_kwargs: Optional[dict] = None,
 ):
@@ -70,7 +69,7 @@ def setup_adapter_training(
             )
         # otherwise, if adapter does not exist, add it
         elif adapter_name not in model.config.adapters:
-            model.add_adapter(adapter_name, config=adapter_config, overwrite_ok=overwrite4adp)
+            model.add_adapter(adapter_name, config=adapter_config)
         # optionally load a pre-trained language adapter
         if adapter_args.load_lang_adapter:
             # resolve the language adapter config
